9ca4b0dac269e6b6d70360d0755e20dfafc77b75
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-08-04 12:55:03 +0530

    Lets start project

---
+++ app/HomeScreen.js
@@ -0,0 +1,12 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-lg text-center p-5">This is our HomeScreen..🔥</Text>
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
